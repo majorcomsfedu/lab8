e8e291eb2f9644cd5dcd6f9d31d92592a3851730
Владислав Латышев <vlatyshev@sfedu.ru>
2023-09-29 18:18:02 +0300

    Готово

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-  print('Привет, мир');
+  var time := ReadInteger('Который час?');
+  assert(time in 0..23);
+  case time of
+    4..10: print('Доброе утро, мир!');
+    11..16: print('Добрый день, мир!');
+    17..22: print('Добрый вечер, мир!')
+  else print('Доброй ночи, мир!');
+  
+  end;
 end.
